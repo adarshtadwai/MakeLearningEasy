df8b1e13c1ed3cf37f537d2662a8887e6a19870c
salahuddin975 <salahuddin975@gmail.com>
2018-03-26 09:58:50 +0600

    change variables name.

--- DynamicProgramming/CoinChangeProblem-NumOfWays.cpp
+++ DynamicProgramming/CoinChangeProblem-NumOfWays.cpp
@@ -7,18 +7,18 @@ number of ways to make up the money.
 #include <iostream>
 #include <cstring>
 
-int count( int coins[], int num_of_coins, int value )
+int count( int coins[], int numOfCoins, int value )
 {
-    int table[value + 1];
+    int numOfWays[value + 1];
 
-    memset(table, 0, sizeof(table));
-    table[0] = 1;
+    memset(numOfWays, 0, sizeof(numOfWays));
+    numOfWays[0] = 1;
 
-    for(int i=0; i<num_of_coins; i++)
+    for(int i=0; i<numOfCoins; i++)
         for(int j=coins[i]; j<=value; j++)
-            table[j] += table[j-coins[i]];
+            numOfWays[j] += numOfWays[j-coins[i]];    // Adding number of ways after using the coin
 
-    return table[value];
+    return numOfWays[value];
 }
 
 
@@ -26,8 +26,8 @@ int main()
 {
     int value = 7;
     int coins[] = {1, 3, 2};
-    int num_of_coins = sizeof(coins)/sizeof(coins[0]);
+    int numOfCoins = sizeof(coins)/sizeof(coins[0]);
 
-    std::cout << "Number of Ways: " << count(coins, num_of_coins, value) << std::endl;
+    std::cout << "Number of Ways: " << count(coins, numOfCoins, value) << std::endl;
     return 0;
 }
